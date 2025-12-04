d58bff5e1c62b77a5cabff88fa6bbbdf5c6dc1e4
rubenjr0 <rubenjr0@proton.me>
2025-12-04 06:38:34 +0100

    Initial

---
+++ src/lib.rs
@@ -0,0 +1,120 @@
+pub fn rate(p1: &Rating, p2: &Rating, outcome: &Outcome, cfg: &SkillrCfg) -> (Rating, Rating) {
+    let d_loc = p1.loc - p2.loc;
+    let s_diff = (p1.scale.powi(2) + p2.scale.powi(2) + 2.0 * cfg.beta.powi(2)).sqrt();
+    let z = _fd(cfg.p_draw, d_loc, s_diff);
+    let p_win = 1.0 - z;
+    let p_lose = _fd(-cfg.p_draw, d_loc, s_diff);
+    let p_draw = z - p_lose;
+    let new_p1 = p1.update(s_diff, [p_win, p_draw, p_lose], outcome, cfg);
+    let new_p2 = p2.update(s_diff, [p_lose, p_draw, p_win], &outcome.inv(), cfg);
+    (new_p1, new_p2)
+}
+
+fn _fd(x: f64, d_loc: f64, s_diff: f64) -> f64 {
+    _sigma((x - d_loc) / s_diff)
+}
+
+fn _sigma(z: f64) -> f64 {
+    (1.0 + (-z).exp()).recip()
+}
+
+pub struct SkillrCfg {
+    loc: f64,
+    scale: f64,
+    beta: f64,
+    tau: f64,
+    p_draw: f64,
+    entropy_rate: f64,
+}
+
+#[derive(Debug)]
+pub struct Rating {
+    loc: f64,
+    scale: f64,
+}
+
+pub enum Outcome {
+    Win,
+    Draw,
+    Lose,
+}
+
+impl Rating {
+    pub fn new(loc: f64, scale: f64) -> Self {
+        Self { loc, scale }
+    }
+
+    fn update(&self, s_diff: f64, ps: [f64; 3], outcome: &Outcome, cfg: &SkillrCfg) -> Self {
+        let (p, o) = match outcome {
+            Outcome::Win => (ps[0], 1.0),
+            Outcome::Draw => (ps[1], 0.0),
+            Outcome::Lose => (ps[2], -1.0),
+        };
+        let expectation = ps[0] - ps[2];
+        let information_gain = -p.ln();
+        let k = self.scale.powi(2) / s_diff.powi(2);
+        let new_loc = self.update_loc(k, o, expectation);
+        let new_scale = self.update_scale(information_gain, cfg);
+        Self {
+            loc: new_loc,
+            scale: (new_scale.powi(2) + cfg.tau.powi(2)).sqrt(),
+        }
+    }
+
+    fn update_loc(&self, k: f64, o: f64, expectation: f64) -> f64 {
+        self.loc + k * (o - expectation)
+    }
+
+    fn update_scale(&self, information_gain: f64, cfg: &SkillrCfg) -> f64 {
+        let entropy = self.scale.ln() + 2.0;
+        let new_entropy = entropy - cfg.entropy_rate * information_gain;
+        (new_entropy - 2.0).exp()
+    }
+}
+
+impl Outcome {
+    fn inv(&self) -> Self {
+        match self {
+            Self::Win => Self::Lose,
+            Self::Draw => Self::Draw,
+            Self::Lose => Self::Win,
+        }
+    }
+}
+
+impl Default for Rating {
+    fn default() -> Self {
+        Self::new(25.0, 25.0 / 3.0)
+    }
+}
+
+impl Default for SkillrCfg {
+    fn default() -> Self {
+        SkillrCfg {
+            loc: 25.0,
+            scale: 25.0 / 3.0,
+            beta: 25.0 / 6.0,
+            tau: 25.0 / 300.0,
+            p_draw: 0.1,
+            entropy_rate: 0.1,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Outcome, Rating, SkillrCfg, rate};
+
+    #[test]
+    fn a() {
+        let p1 = Rating::default();
+        let p2 = Rating::default();
+        let cfg = SkillrCfg::default();
+        let (new_p1, new_p2) = rate(&p1, &p2, &Outcome::Win, &cfg);
+
+        assert!(new_p1.loc > p1.loc);
+        assert!(new_p2.loc < p2.loc);
+        assert!(new_p1.scale < p1.scale);
+        assert!(new_p2.scale < p2.scale);
+    }
+}
